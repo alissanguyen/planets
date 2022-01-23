85272b625d9065f80b1b5400c3a8cca048a095c6
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-01-23 12:59:04 -0800

    Reorganizing codes and template

---
+++ src/components/Navbar/NavMobile.js
@@ -0,0 +1,80 @@
+import { AnimatePresence } from "framer-motion";
+import * as React from "react";
+import { Nav, Menu, Bars, List } from "../Navbar/styles/NavMobileStyles";
+import useToggleMenu from "../../utils/useToggleMenu";
+import { Item, Link } from "../Navbar/styles/NavMobileStyles";
+import Icon from "../Icon/Icon";
+import { planets } from "../../constants/data.js";
+
+const NavMobile = ({ windowWidth }) => {
+  const [handleToggle, restoreToDefault, isOpen, isExpanded, tabletBreakpoint] =
+    useToggleMenu();
+
+  function handleResize() {
+    if (windowWidth >= tabletBreakpoint) {
+      restoreToDefault();
+    } else {
+      return;
+    }
+  }
+  React.useEffect(() => {
+    handleResize();
+  }, [windowWidth]);
+
+  return (
+    <Nav>
+      <Menu
+        aria-label="Menu toggle"
+        aria-expanded={isExpanded}
+        aria-controls="nav-list"
+        onClick={handleToggle}
+      >
+        <Bars isOpen={isOpen} />
+      </Menu>
+      <AnimatePresence>
+        {isOpen && (
+          <List
+            id="nav-list"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "linear", duration: 0.3 }}
+            exit={{ opacity: 0 }}
+          >
+            <Items restoreToDefault={restoreToDefault} />
+          </List>
+        )}
+      </AnimatePresence>
+    </Nav>
+  );
+};
+
+const Items = ({ restoreToDefault }) => {
+  return planets.map((planet) => (
+    <Item
+      key={planet.id}
+      initial={{ x: `100vw` }}
+      animate={{ x: "0vw" }}
+      transition={{
+        ease: [0.06, 0.9, 1, 0.98],
+        duration: 0.7,
+        delay: `${(planet.id * 5 + 0.5) / 100}`,
+      }}
+    >
+      <Link
+        to={planet.path}
+        color={planet.color}
+        onClick={() => restoreToDefault()}
+      >
+        {planet.name}
+        <Icon
+          name="icon-chevron1"
+          size={20}
+          color="hsl(240, 6%, 54%)"
+          customStyle={{ marginTop: "5px" }}
+        />
+      </Link>
+    </Item>
+  ));
+};
+
+export default NavMobile;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import {
+  Header,
+  Logo,
+  LogoLink,
+  Container,
+} from "../Navbar/styles/NavbarStyles";
+import NavMobile from "./NavMobile";
+import NavDesktop from "./NavDesktop";
+
+const Navbar = ({ pathName, activePlanet, onHover }) => {
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+  const tabletBreakpoint = 768;
+
+  React.useEffect(() => {
+    window.addEventListener("resize", () => setWindowWidth(window.innerWidth));
+  }, [windowWidth]);
+
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: { delay: 1.5, duration: 4 },
+    },
+    exit: {
+      opacity: 0,
+      transition: { duration: 1 },
+    },
+  };
+
+  return (
+    <Header
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <Container>
+        <Logo>
+          <LogoLink to="/">The Planets</LogoLink>
+        </Logo>
+        {windowWidth >= tabletBreakpoint ? (
+          <NavDesktop
+            pathName={pathName}
+            onHover={onHover}
+            activePlanet={activePlanet}
+          />
+        ) : (
+          <NavMobile windowWidth={windowWidth} />
+        )}
+      </Container>
+    </Header>
+  );
+};
+
+export default Navbar;
